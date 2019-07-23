8f46cbccf44a52778719ef378bbd2e641dea5b13
RYAN WEBB <webbrr@ph.ibm.com>
2019-07-23 09:51:27 +0800

    Clean up unneeded files

--- src/main/java/io/ryanwebb/utils/InputFormatter.java
+++ src/main/java/io/ryanwebb/utils/InputFormatter.java
@@ -1,8 +1,16 @@
 package io.ryanwebb.utils;
 
+/**
+ * Base for formatter, depending on the programmer on how to implement.
+ */
 public interface InputFormatter 
 {
 
+	/**
+	 * Format city string before sending to the API
+	 * @param city
+	 * @return formatted string result
+	 */
 	public String formatCity(final String city);
 
 }
